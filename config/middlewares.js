74fe881f3a1360a32ee002bf9731526dba5357ed
Sadraque Santos <sdq.sts@gmail.com>
2018-10-21 16:52:23 +0000

    added front-end in vue

--- config/middlewares.js
+++ config/middlewares.js
@@ -3,19 +3,23 @@ const bodyParser = require('body-parser')
 const helmet = require('helmet')
 const morgan = require('morgan')
 const cors = require('cors')
+const path = require('path')
+const compression = require('compression')
 const authStrategy = require('../auth')
 
 module.exports = () => ({
   config (app) {
     const authentication = authStrategy(app)
+    const clientPath = path.join(__dirname, '../client/dist')
 
     app.set('port', process.env.PORT || 7777)
     app.use(bodyParser.json())
-    app.use(express.static('public'))
-    app.use(helmet())
+    app.use(express.static(clientPath))
     app.use(cors())
-    app.use(authentication.initialize())
+    app.use(helmet())
+    app.use(compression())
     app.use(morgan('dev'))
+    app.use(authentication.initialize())
 
     app.auth = authentication
   }
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
+const express = require('express')
 const auth = require('../api/Auth')
 const users = require('../api/Users')
+const path = require('path')
 
 module.exports = (app) => ({
   init () {
-    app.use('/api/v1/auth', auth(app))
+    const clientPath = path.join(__dirname, '../client/dist')
+
     app.use('/api/v1/users', users(app))
+    app.use('/api/v1/auth', auth(app))
+    app.use('/', express.static(clientPath))
+    app.use('*', express.static(clientPath))
   }
 })
