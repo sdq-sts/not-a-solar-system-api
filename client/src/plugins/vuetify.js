7d181d1442db5e36765e8f3988eea734341b32ed
Sadraque <sdq.sts@gmail.com>
2019-01-09 01:47:54 -0200

    created a vuetify theme

--- client/src/plugins/vuetify.js
+++ client/src/plugins/vuetify.js
@@ -2,6 +2,17 @@ import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
 import 'vuetify/src/stylus/app.styl'
 
+// Helpers
+import colors from 'vuetify/es5/util/colors'
+
 Vue.use(Vuetify, {
-  iconfont: 'md'
+  iconfont: 'md',
+  theme: {
+    primary: colors.blue.lighten2, // #64B5F6
+    secondary: colors.red.lighten4, // #FFCDD2
+    accent: colors.indigo.base, // #3F51B5
+    success: colors.green.accent2, // #69F0AE
+    danger: colors.red.accent2, // #FF5252
+    warning: colors.yellow.accent2 // #FFFF00
+  }
 })
