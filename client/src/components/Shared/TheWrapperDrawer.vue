32b12659477bdf5d1b377607b404c8618f6483ff
Sadraque <sdq.sts@gmail.com>
2019-02-07 14:32:25 -0200

    current route is now highlighted in menu

--- client/src/components/Shared/TheWrapperDrawer.vue
+++ client/src/components/Shared/TheWrapperDrawer.vue
@@ -1,42 +1,17 @@
 <template>
   <v-list>
-    <template v-for="(item, i) in items">
-      <v-list-group
-        v-if="item.children"
-        v-model="item.model"
-        :key="i"
-        :group="item.pathName"
-        :prepend-icon="item.model ? item.prependIcon : item.prependIconAlt"
-      >
-        <v-list-tile slot="activator">
-          <v-list-tile-content>
-            <v-list-tile-title>{{ item.text }}</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-
-        <v-list-tile
-          v-for="(child, i) in item.children"
-          :key="i"
-          @click="changePath(child)"
-        >
-          <v-list-tile-action v-if="child.icon">
-            <v-icon>{{ child.icon }}</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>{{ child.text }}</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list-group>
-
-      <v-list-tile v-else :key="item.text" @click="changePath(item)">
-        <v-list-tile-action>
-          <v-icon>{{ item.icon }}</v-icon>
-        </v-list-tile-action>
-        <v-list-tile-content>
-          <v-list-tile-title>{{ item.text }}</v-list-tile-title>
-        </v-list-tile-content>
-      </v-list-tile>
-    </template>
+    <v-list-tile
+      v-for="(item, i) in items"
+      :key="i"
+      :to="item.path"
+    >
+      <v-list-tile-action>
+        <v-icon>{{ item.icon }}</v-icon>
+      </v-list-tile-action>
+      <v-list-tile-content>
+        <v-list-tile-title>{{ item.text }}</v-list-tile-title>
+      </v-list-tile-content>
+    </v-list-tile>
   </v-list>
 </template>
 
@@ -44,19 +19,12 @@
 export default {
   data: () => ({
     items: [
-      { text: 'Dashboard', icon: 'assessment', pathName: 'dashboard.index' },
-      { text: 'Vendas', icon: 'trending_up', pathName: 'manage.sales' },
-      { text: 'Compras', icon: 'credit_card', pathName: 'manage.purchases' },
-      { text: 'Produtos', icon: 'view_module', pathName: 'manage.products' }
+      { text: 'Dashboard', icon: 'assessment', path: '/dashboard' },
+      { text: 'Vendas', icon: 'trending_up', path: '/vendas' },
+      { text: 'Compras', icon: 'credit_card', path: '/compras' },
+      { text: 'Produtos', icon: 'view_module', path: '/produtos' }
     ]
-  }),
-
-  methods: {
-    changePath (item) {
-      const { pathName } = item
-      this.$router.push({ name: pathName })
-    }
-  }
+  })
 }
 </script>
 
