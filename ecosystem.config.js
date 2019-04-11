ffe1169fcdf7a6400dbddb1fafe1d81b79935b04
Sadraque <sdq.sts@gmail.com>
2019-04-11 17:38:44 -0300

    modified pm2 to run in 1 core

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     name: 'nass',
     script: 'server.js',
     exec_mode: 'cluster',
-    instances: -1,
+    instances: 1,
     autorestart: true,
     max_memory_restart: '200M',
     watch: false,
