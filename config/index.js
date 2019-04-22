4f866fbfea9e46e15d429387f176bfb6992229f9
Sadraque <sdq.sts@gmail.com>
2019-04-22 13:45:33 -0300

    simplified error handlers

--- config/index.js
+++ config/index.js
@@ -3,12 +3,10 @@ require('./logger')
 require('./handleErrors')
 
 const { initServices } = require('@/services')
-// const { handleErrors } = require('./handleErrors')
 const { setVariables } = require('./variables')
 const { applyDatasource } = require('./datasource')
 
 module.exports.setAppConfig = (app) => {
-  // handleErrors()
   setVariables(app)
   initServices(app)
   applyDatasource(app)
